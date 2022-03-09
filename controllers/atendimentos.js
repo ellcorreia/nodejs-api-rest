b53238202014481a3271a2c7c1497e744f82ad97
Gabriel Correia <thecorreia@me.com>
2022-03-08 23:25:40 -0300

    first commit

---
+++ controllers/atendimentos.js
@@ -0,0 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos - GET'))  
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Você está na rota de atendimento - POST')
+    })
+}
